2f43772d5129d126ebdc8fdf60734129830c63e1
Роман <roninmtn@gmail.com>
2025-08-21 22:10:32 +0400

    &&&

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,71 @@
-import java.util.Arrays;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        // Задание 1, 2
         int [] weight = new int [12];
         weight[0] = 1;
         weight[1] = 2;
         weight[2] = 3;
-        for(int i = 0; i < weight.length; i++) {
-            System.out.printf((weight[i]) + ", ");
-        }
-        System.out.println(" ");
-        for(int i1 = weight.length-1; i1 >=0; i1--) {
-            System.out.printf((weight[i1]) + ", ");
+        for(int i = 0; i <=weight.length-1; i++) {
+            System.out.printf(String.valueOf(weight[i]));
+
+            if (i < weight.length-1) {
+                System.out.printf(", ");
+            }
         }
         System.out.println(" ");
 
         double [] weight2 = {1.57, 7.654, 9.986, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        for(int i2 = 0; i2 < weight2.length; i2++) {
-            System.out.printf((weight2[i2]) + ", ");
-        }
-        System.out.println(" ");
+        for(int i2 = 0; i2 <=weight2.length-1; i2++) {
+            System.out.printf(String.valueOf(weight2[i2]));
 
-            for(int i22 = weight2.length-1; i22 >=0; i22--){
-            System.out.printf((weight2[i22])+", ");
+            if (i2 < weight2.length-1) {
+                System.out.printf(", ");
+            }
         }
 
         System.out.println(" ");
 
         int[] cats = {4, 8, 9, 5, 0, 0, 0, 4, 0, 0, 11, 0, 0};
-        for(int i3 = 0; i3 < cats.length; i3++) {
-            System.out.printf(cats[i3]+", ");
+        for(int i3 = 0; i3 <=cats.length-1; i3++) {
+            System.out.printf(String.valueOf(cats[i3]));
+
+            if (i3 < cats.length-1) {
+                System.out.printf(", ");
+            }
         }
+        System.out.println("\n\nЗадание №3");
+
+        //Задание №3
+        for(int i = weight.length -1; i >=0; i--) {
+            System.out.printf(String.valueOf(weight[i]));
+
+            if (i > 0) {
+                System.out.printf(", ");
+            }
+        }
+        System.out.println(" ");
+        for(int i22 = weight2.length -1; i22 >=0; i22--) {
+            System.out.printf(String.valueOf(weight2[i22]));
+
+            if (i22 > 0) {
+                System.out.printf(", ");
+            }
+        }
+
         System.out.println(" ");
 
-        for(int i4 = cats.length-1; i4 >= 0; i4-- ) {
-            System.out.printf(cats[i4] + ", ");
+
+        for(int i4 = cats.length -1; i4 >=0; i4--) {
+            System.out.printf(String.valueOf(cats[i4]));
+
+            if (i4 > 0) {
+                System.out.printf(", ");
+            }
         }
-        System.out.println(" ");
+        System.out.println("\n\nЗадание №4");
 
         int[] arr = {1, 2, 3};
             for(int i5 = 0; i5<arr.length; i5++) {
