6e0d5d3b8897340034bbe2cf89b81578ed1fdfa5
Роман <roninmtn@gmail.com>
2025-08-24 18:13:04 +0400

    Лягушайник

--- src/Main.java
+++ src/Main.java
@@ -76,5 +76,58 @@ public class Main {
             }
         }
         System.out.printf(Arrays.toString(arr));
+
+        //Часть 2
+        System.out.println("\n\n\nЧасть 2\nЗадача №1");
+
+        //Задача №1
+
+        int[] salary2 = new int[5];
+        salary2[0] = 20000;
+        salary2[1] = 10000;
+        salary2[2] = 5000;
+        salary2[3] = 10000;
+        salary2[4] = 8000;
+        int sum = 0;
+        for (int salary = 0; salary < salary2.length; salary++) {
+            sum += salary2[salary];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        System.out.println("\n\nЗадача №2");
+
+        //Задача №2
+        int[] expensesWeek = {2000, 3000, 16000, 2600, 500};
+        int maxExpensesWeek = -1;
+        int min = 10000;
+        for (final int current : expensesWeek) {
+            if (current > maxExpensesWeek) {
+                maxExpensesWeek = current;
+            }
+        }
+        System.out.println("Максимальная сумма трат за неделю составила " +maxExpensesWeek +" рублей");
+
+        for (final int current1 : expensesWeek) {
+            if(current1 < min) {
+                min = current1;
+            }
+        }
+        System.out.println("Минимальная сумма трат за неделю составила " + min +" рублей");
+
+        System.out.println("\n\nЗадание №3");
+        int[] mony = {10000, 2000, 5600, 3070, 7000};
+        int sum2 = 0;
+        for (int week = 0; week < mony.length; week++) {
+                sum2 += mony[week];
+
+        }
+        sum2 = sum2/mony.length ;
+        System.out.println(" Средняя сумма трат за месяц составила "+sum2+" рублей");
+        System.out.println("\n\nЗадание №4\n");
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int f = reverseFullName.length - 1; f >= 0; f--) {
+            System.out.printf(String.valueOf(reverseFullName[f]));
+        }
     }
 }
